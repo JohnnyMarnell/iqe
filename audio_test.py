78baa7d4862474d36efab818c6b999424f960a4c
Johnny Marnell <johnnymarnell@gmail.com>
2023-04-27 09:55:54 -0400

    Some notes, re-org

--- audio_test.py
+++ audio_test.py
@@ -1,5 +1,5 @@
 import numpy as np, sounddevice as sd
-import time, threading, rtmidi2, math, scipy.signal
+import time, threading, rtmidi2, math, scipy.signal, os
 from typing import NamedTuple
 
 class Block(NamedTuple): index: int; size: int; start: int; end: int; wall_time: float; stream_time: float; \
@@ -50,61 +50,74 @@ def stream_callback(indata, outdata, num_frames, block_time, status):
     block_index = block_index + 1
 
 
-""" Look back in time (max 6 seconds) and look at silence stretches to infer tempo """
+""" Simple method to look at stretches of silence to infer bpm and phase (tempo) """
+def simple_detect_tempo(window):
+    # Build silence ranges, make array that is 1 where sample is 0 (silent), pad ends
+    iszero = np.concatenate(([0], np.equal(window, 0), [0]))
+    absdiff = np.abs(np.diff(iszero))
+    # Runs start and end where absdiff is 1.
+    zero_runs = np.where(absdiff == 1)[0].reshape(-1, 2)
+    zero_runs = list(map(list, zero_runs))
+    
+    # Make sure we have at least 4, and use inner (since outers can be incomplete)
+    if len(zero_runs) >= 4:
+        silence_end = zero_runs[2][1]
+        silence_start = zero_runs[2][0]
+        prev_silence_end = zero_runs[1][1]
+        spb = (silence_end - prev_silence_end) / sr
+        beep_len = (silence_start - prev_silence_end + 1) / sr
+        print("Detected BPM {} and beep length {}s, secs per beat {}, a beat sample # {}".format(60 / spb, beep_len, spb, silence_end))
+        return (60 / spb, silence_end)
+    return (None, None)
+
+
+""" Try detecting tempo (look back max 6 seconds), and schedule metronome click """
 def tempo_detect_thread():
     print("Starting tempo detection thread")
     def tick():
         global beat_time, spb
         while running:
-            if beat_time is None:
-                bidx = block_index - 1
-                sample_end = bidx * block_size + block_size
-                sample_start = max(0, sample_end - math.ceil(6 * sr))
-                window = np.empty(sample_end - sample_start)
-                for i in range(sample_start, sample_end):
-                    window[i - sample_start] = samples[i % SAMPLES_SZ]
+            bidx = block_index - 1
+            
+            # Build numpy window of samples back maximum 6 seconds
+            sample_end = bidx * block_size + block_size
+            sample_start = max(0, sample_end - math.ceil(6 * sr))
+            window = np.empty(sample_end - sample_start)
+            for i in range(sample_start, sample_end):
+                window[i - sample_start] = samples[i % SAMPLES_SZ]
+            
+            bpm, beat_sample_index_in_window = simple_detect_tempo(window)
+
+            if bpm:
+                # Using stream time and sample buffer index, get stream time of the beat, advance to next
+                spb = 60 / bpm
+                beat_sample = sample_start + beat_sample_index_in_window
+                beat_block = blocks[math.floor(beat_sample / block_size) % BLOCK_TIMES_SZ]
+                beat_time = beat_block.inputBufferAdcTime + (beat_sample - beat_block.start) / sr
+                print("Beat occured at sample {} of block # {}, ranged [{}, {}) over {} samples == {} block size, stream time {}".format(
+                    beat_sample, beat_block.index, beat_block.start, beat_block.end,
+                    beat_block.end - beat_block.start, block_size, beat_time
+                ))
+
+                # Cook / fudge time seconds by output latency only (shrug) and current DAW settings (double shrug)
+                beat_time = beat_time - audio_stream.latency[1]
+                beat_time = beat_time - float(os.environ.get("IQE_LATENCY_SHIFT") or (2 * 64 / 48000))
                 
-                # Build silence ranges, make array that is 1 where sample is 0 (silent), pad ends
-                iszero = np.concatenate(([0], np.equal(window, 0), [0]))
-                absdiff = np.abs(np.diff(iszero))
-                # Runs start and end where absdiff is 1.
-                zero_runs = np.where(absdiff == 1)[0].reshape(-1, 2)
-                zero_runs = list(map(list, zero_runs))
+                # Set two beats in the future for metronome to have time to click
+                beat_time = beat_time + (math.ceil((audio_stream.time - beat_time) / spb) + 1) * spb
                 
-                # Make sure we have at least 4, and use inner (since outers can be incomplete)
-                if len(zero_runs) >= 4:
-                    silence_end = zero_runs[2][1]
-                    silence_start = zero_runs[2][0]
-                    prev_silence_end = zero_runs[1][1]
-                    spb = (silence_end - prev_silence_end) / sr
-                    beep_len = (silence_start - prev_silence_end + 1) / sr
-                    print("Detected BPM {} and beep length {}s, secs per beat {}".format(60 / spb, beep_len, spb))
-                    
-                    # Using stream time and sample buffer index, get stream time of the beat, advance to next
-                    beat_sample = sample_start + silence_end
-                    beat_block = blocks[math.floor(beat_sample / block_size) % BLOCK_TIMES_SZ]
-                    beat_time = beat_block.inputBufferAdcTime + (beat_sample - beat_block.start) / sr
-                    print("Beat occured at sample {} of block # {}, ranged [{}, {}) over {} samples == {} block size, stream time {}".format(
-                        beat_sample, beat_block.index, beat_block.start, beat_block.end,
-                        beat_block.end - beat_block.start, block_size, beat_time
-                    ))
-                    # Cook / fudge time number by output latency only (shrug) and current DAW settings (double shrug)
-                    beat_time = beat_time - audio_stream.latency[1] - 2 * 64 / 48000
-                    beat_time = beat_time + (math.ceil((audio_stream.time - beat_time) / spb) + 1) * spb
-                    
-                    # debug
-                    print("\n\nDebug info about last 10 seconds of blocks:")
-                    idiffCounts = {}
-                    for i in range(max(1, bidx - math.ceil(10 * sr / block_size)), bidx + 1):
-                        block = blocks[i % BLOCK_TIMES_SZ]
-                        prev = blocks[(i - 1) % BLOCK_TIMES_SZ]
-                        idiff = block.inputBufferAdcTime - prev.inputBufferAdcTime
-                        if (idiff < 0):
-                            print("Strange, input clock went back in time for these blocks...")
-                            print(prev)
-                            print(block)
-                        idiffCounts[idiff] = (idiffCounts.get(idiff) or 0) + 1
-                    print("Counts of differences of subsequent input clocks:", idiffCounts)
+                # Debug
+                print("\n\n ######## Debug info for last 40 blocks:")
+                idiffCounts = {}
+                for i in range(max(1, bidx - 40), bidx + 1):
+                    block = blocks[i % BLOCK_TIMES_SZ]
+                    prev = blocks[(i - 1) % BLOCK_TIMES_SZ]
+                    idiff = block.inputBufferAdcTime - prev.inputBufferAdcTime
+                    if (idiff < 0):
+                        print("**** STRANGE, input clock went back in time for these blocks...\n", prev, "\n", block)
+                    idiffCounts[idiff] = (idiffCounts.get(idiff) or 0) + 1
+                print("Counts of differences of subsequent input clocks:", idiffCounts, "\n\n")
+                break
             time.sleep(0.300)
     return threading.Thread(target=tick)
 
@@ -132,24 +145,15 @@ def metronome_click_thread():
     return threading.Thread(target=tick)
 
 
-# audio_stream = sd.Stream(samplerate=sr, channels=(2,2), callback=stream_callback, blocksize=128,
-#                          device=(thru_in['index'], default_out['index']), latency=0)
-# audio_stream = sd.Stream(samplerate=sr, channels=(2,2), callback=stream_callback, blocksize=256,
-#                          device=(thru_in['index'], default_out['index']), latency=0)
-# audio_stream = sd.Stream(samplerate=sr, channels=(2,2), callback=stream_callback, blocksize=1024,
-#                          device=(thru_in['index'], default_out['index']), latency=0)
-# audio_stream = sd.Stream(samplerate=sr, channels=(2,2), callback=stream_callback, blocksize=64,
-#                          device=(thru_in['index'], default_out['index']), latency='low')
-# audio_stream = sd.Stream(samplerate=sr, channels=(2,2), callback=stream_callback, blocksize=1024,
-#                          device=(thru_in['index'], default_out['index']), latency='high')
-# audio_stream = sd.Stream(samplerate=sr, channels=(2,2), callback=stream_callback, blocksize=128,
-#                          device=(thru_in['index'], default_out['index']), latency='high')
+# ****************************************
+# Weirdly, with a 'high' latency (thus should be more stable?) I see clock goes back in time, every ~13 blocks...
 audio_stream = sd.Stream(samplerate=sr, channels=(2,2), callback=stream_callback, blocksize=1024,
-                         device=(thru_in['index'], default_out['index']), latency='low')
-# audio_stream = sd.Stream(samplerate=sr, channels=(2,2), callback=stream_callback, blocksize=512,
+                         device=(thru_in['index'], default_out['index']), latency='high')
+# ****************************************
+
+# Maybe stable / maybe accurate? Wish the block size didn't need to be so high (especially compared to DAW performance)
+# audio_stream = sd.Stream(samplerate=sr, channels=(2,2), callback=stream_callback, blocksize=1024,
 #                          device=(thru_in['index'], default_out['index']), latency='low')
-# audio_stream = sd.Stream(samplerate=sr, channels=(2,2), callback=stream_callback, blocksize=64,
-#                          device=(thru_in['index'], default_out['index']), latency='high')
 with audio_stream:
     block_size = audio_stream.blocksize
     print("Stream processing started, I+O latency secs, blocksize:\n", audio_stream.latency, audio_stream.blocksize)
@@ -169,14 +173,31 @@ Notes:
 - BPM and beep length calculations are accurate when I change in DAW
 - DAW has low buffer settings (64) and low perceivable latency (e.g. playing midi instrument along),
   so I don't think that's much of a factor?
-- The metronome clicks sound uniformly spaced, plus do not drift, so I don't think there's threading timing problem
+- The metronome clicks sound uniformly spaced, plus do not drift, so I don't think there's threading + timing problem
 - I get best results (I think?) with subtracting output latency + perceived DAW latency, 
 - Are my assumptions about the cdata struct times all wrong?
     - inputBufferAdcTime: the stream.time when this block_size number of samples started conversion, thus was read from input,
-      or "heard", and n-th sample in the buffer must have played at n/sr seconds later, or it would've sounded glitchy / weird?
-    - How could the inputBufferAdcTime *decrease* (implying earlier?) in the next block processed?
+      or "heard", and n-th sample in the buffer must have processed at n/sr seconds later,
+      or it would've sounded glitchy / weird?
+    - How could the inputBufferAdcTime *decrease* (implying earlier?) in the next block processed? Bug?
     - The currentTime is the time this block is being processed / handled / called? I would assume this would vary, but shouldn't
-      the input time, since it's reading one same size block of samples after another, shouldn't its times be uniformly spaced by those
-      block_size/sr seconds, otherwise it would've sounded weird / xruns?
+      the input time, since it's reading one same-sized block of samples after another, be uniformly spaced by those
+      block_size/sr seconds every time, otherwise it would've sounded weird / xruns?
 
 """
+
+# Tried a lot of combos, xruns, and unexpected results :(
+# audio_stream = sd.Stream(samplerate=sr, channels=(2,2), callback=stream_callback, blocksize=128,
+#                          device=(thru_in['index'], default_out['index']), latency=0)
+# audio_stream = sd.Stream(samplerate=sr, channels=(2,2), callback=stream_callback, blocksize=256,
+#                          device=(thru_in['index'], default_out['index']), latency=0)
+# audio_stream = sd.Stream(samplerate=sr, channels=(2,2), callback=stream_callback, blocksize=1024,
+#                          device=(thru_in['index'], default_out['index']), latency=0)
+# audio_stream = sd.Stream(samplerate=sr, channels=(2,2), callback=stream_callback, blocksize=64,
+#                          device=(thru_in['index'], default_out['index']), latency='low')
+# audio_stream = sd.Stream(samplerate=sr, channels=(2,2), callback=stream_callback, blocksize=128,
+#                          device=(thru_in['index'], default_out['index']), latency='high')
+# audio_stream = sd.Stream(samplerate=sr, channels=(2,2), callback=stream_callback, blocksize=512,
+#                          device=(thru_in['index'], default_out['index']), latency='low')
+# audio_stream = sd.Stream(samplerate=sr, channels=(2,2), callback=stream_callback, blocksize=64,
+#                          device=(thru_in['index'], default_out['index']), latency='high')
