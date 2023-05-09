7a1e6991e2f052187e2613ab7cf1ef2b9c030991
Johnny Marnell <johnnymarnell@gmail.com>
2023-05-09 13:14:29 -0400

    Web app + full sync working!!!

--- src/main/java/org/iqe/LXPluginIQE.java
+++ src/main/java/org/iqe/LXPluginIQE.java
@@ -3,12 +3,15 @@ package org.iqe;
 import heronarts.lx.*;
 import heronarts.lx.color.LXColor;
 import heronarts.lx.model.LXPoint;
+import heronarts.lx.modulation.LXModulationEngine;
+import heronarts.lx.osc.LXOscComponent;
+import heronarts.lx.parameter.LXParameter;
 import heronarts.lx.pattern.LXPattern;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class LXPluginIQE implements LXPlugin {
+    protected LX lx;
 
     @Override
     public void dispose() {
@@ -17,8 +20,39 @@ public class LXPluginIQE implements LXPlugin {
 
     @Override
     public void initialize(LX lx) {
-        System.out.println("*** LXP init");
+        this.lx = lx;
         lx.registry.addPattern(HolyTrinitiesPattern.class);
         lx.registry.addPattern(BassBreathPattern.class);
+
+        // IMHO it's great this exists, and should be made public and happen by default on project load
+        lx.addProjectListener((file, change) -> {
+            if (change == LX.ProjectListener.Change.OPEN) {
+                oscQueryAll(lx);
+            }
+        });
+    }
+
+    // IMHO it's great this exists, and should be made public and happen by default on project load
+    public static void oscQueryAll(LX lx) {
+        int index = 1;
+        LXComponent component;
+        Set<Integer> visited = new HashSet<>();
+        while ((component = lx.getComponent(index)) != null) {
+            oscQuery(visited, lx, component);
+            index++;
+        }
+    }
+
+    // IMHO it's great this exists, and should be made public and happen by default on project load
+    public static void oscQuery(Set<Integer> visited, LX lx, LXComponent component) {
+        if (!visited.contains(component.getId())) {
+            System.out.println("sigh " + component.getId() + " " + component.getLabel() + " " + component.getClass());
+            visited.add(component.getId());
+            component.getParameters().forEach(lx.engine.osc::sendParameter);
+            if (component instanceof LXModulationEngine) {
+                ((LXModulationEngine) component).getModulators().forEach(m -> oscQuery(visited, lx, m));
+            }
+            component.children.values().forEach(c -> oscQuery(visited, lx, component));
+        }
     }
 }
