7a92f9aa120eccb4bd38db2770c2dace5cf839e1
Johnny Marnell <johnnymarnell@gmail.com>
2023-05-22 10:34:28 -0700

    Add pillars, total model strips increasing 69 to 75

--- buildGeo.js
+++ buildGeo.js
@@ -6,7 +6,7 @@ let id = 100
 const stripLen = 700 // 5 spacing * 140 LED pixels / "numPoints"
 const s = defaultNagBugglerSaberOfLight
 const sl = stripLenMultiplier => stripLenMultiplier * stripLen
-const numPillars = 15
+const numPillars = 21
 
 function buildNagBugglerSaberOfLightFixtures() {
     return [
@@ -18,31 +18,40 @@ function buildNagBugglerSaberOfLightFixtures() {
             e.g. pc4-s2 means the second pillar progressing southward through 4th pillar column
             Per the data/power diagrams, West to East progression in general increases numerically (e.g. squares 1 - 3)
 
-            PILLARS #0 - #14:
-            Going east to west, 4 pillar columns of 3 south to north (x+)
+            PILLARS #0 - #20:
+            Going east to west, 7 pillar columns of 3 south to north (x+)
+            temp: 4, 3, 2, 1 becomes 7, 5, 3, 1
         */
-        {...s({roll: -90, x: sl(0  ), z: sl(0 * 3)}, 'pc4 pc4-s3 ps ps-e4 po po-cw6 sq3 sq3-cw4')},
-        {...s({roll: -90, x: sl(1.5), z: sl(0 * 3)}, 'pc4 pc4-s2 pm pm-e7 po po-cw5 sq3 sq3-cw3')},
-        {...s({roll: -90, x: sl(3  ), z: sl(0 * 3)}, 'pc4 pc4-s1 pn pn-e4 po po-cw4 sq3 sq3-cw2')},
+        {...s({roll: -90, x: sl(0  ), z: sl(0 * 1.5)}, 'pc7 pc7-s3 ps ps-e7 po po-cw9 sq3 sq3-cw5')},
+        {...s({roll: -90, x: sl(1.5), z: sl(0 * 1.5)}, 'pc7 pc7-s2 pm pm-e7 po po-cw8 sq3 sq3-cw4')},
+        {...s({roll: -90, x: sl(3  ), z: sl(0 * 1.5)}, 'pc7 pc7-s1 pn pn-e7 po po-cw7 sq3 sq3-cw3')},
 
-        {...s({roll: -90, x: sl(0  ), z: sl(1 * 3)}, 'pc3 pc3-s3 ps ps-e3 po po-cw7 sq3 sq3-cw5 sq2 sq2-cw4')},
-        {...s({roll: -90, x: sl(1.5), z: sl(1 * 3)}, 'pc3 pc3-s2 pm pm-e5 pi pi-e4  sq3 sq3-cw6 sq2 sq2-cw3')},
-        {...s({roll: -90, x: sl(3  ), z: sl(1 * 3)}, 'pc3 pc3-s1 pn pn-e3 po po-cw3 sq3 sq3-cw1 sq2 sq2-cw2')},
+        {...s({roll: -90, x: sl(0  ), z: sl(1 * 1.5)}, 'pc6 pc6-s3 ps ps-e6 po po-cw10 sq3 sq3-cw6')},
+        {...s({roll: -90, x: sl(1.5), z: sl(1 * 1.5)}, 'pc6 pc6-s2 pm pm-e6 pi pi-e5   sq3        ')},
+        {...s({roll: -90, x: sl(3  ), z: sl(1 * 1.5)}, 'pc6 pc6-s1 pn pn-e6 po po-cw6  sq3 sq3-cw2')},
 
-        {...s({roll: -90, x: sl(0  ), z: sl(2 * 3)}, 'pc2 pc2-s3 ps ps-e4 po po-cw8 sq2 sq2-cw5 sq1 sq1-cw4')},
-        {...s({roll: -90, x: sl(1.5), z: sl(2 * 3)}, 'pc2 pc2-s2 pm pm-e3 pi pi-e2  sq2 sq2-cw6 sq1 sq1-cw3')},
-        {...s({roll: -90, x: sl(3  ), z: sl(2 * 3)}, 'pc2 pc2-s1 pn pn-e2 po po-cw2 sq2 sq2-cw1 sq1 sq1-cw2')},
+        {...s({roll: -90, x: sl(0  ), z: sl(2 * 1.5)}, 'pc5 pc5-s3 ps ps-e5 po po-cw11 sq3 sq3-cw7 sq2 sq2-cw5')},
+        {...s({roll: -90, x: sl(1.5), z: sl(2 * 1.5)}, 'pc5 pc5-s2 pm pm-e5 pi pi-e4   sq3 sq3-cw8 sq2 sq2-cw4')},
+        {...s({roll: -90, x: sl(3  ), z: sl(2 * 1.5)}, 'pc5 pc5-s1 pn pn-e5 po po-cw5  sq3 sq3-cw1 sq2 sq2-cw3')},
 
-        {...s({roll: -90, x: sl(0  ), z: sl(3 * 3)}, 'pc1 pc1-s3 ps ps-e1 po po-cw9  sq1 sq1-cw5')},
-        {...s({roll: -90, x: sl(1.5), z: sl(3 * 3)}, 'pc1 pc1-s2 pm pm-e1 po po-cw10 sq1 sq1-cw6')},
-        {...s({roll: -90, x: sl(3  ), z: sl(3 * 3)}, 'pc1 pc1-s1 pn pn-e1 po po-cw1  sq1 sq1-cw1')},
+        {...s({roll: -90, x: sl(0  ), z: sl(3 * 1.5)}, 'pc4 pc4-s3 ps ps-e4 po po-cw12 sq2 sq2-cw6')},
+        {...s({roll: -90, x: sl(1.5), z: sl(3 * 1.5)}, 'pc4 pc4-s2 pm pm-e4 pi pi-e3   sq2        ')},
+        {...s({roll: -90, x: sl(3  ), z: sl(3 * 1.5)}, 'pc4 pc4-s1 pn pn-e4 po po-cw4  sq2 sq2-cw2')},
 
-        // then 3 at the rafter diagonal X intersections
-        {...s({roll: -90, x: sl(1.5), z: sl((1.5 + 0 * 3))}, 'pm pm-e6 pi pi-e5 sq3')},
-        {...s({roll: -90, x: sl(1.5), z: sl((1.5 + 1 * 3))}, 'pm pm-e4 pi pi-e3 sq2')},
-        {...s({roll: -90, x: sl(1.5), z: sl((1.5 + 2 * 3))}, 'pm pm-e2 pi pi-e1 sq1')},
+        {...s({roll: -90, x: sl(0  ), z: sl(4 * 1.5)}, 'pc3 pc3-s3 ps ps-e3 po po-cw13 sq2 sq2-cw7 sq1 sq1-cw5')},
+        {...s({roll: -90, x: sl(1.5), z: sl(4 * 1.5)}, 'pc3 pc3-s2 pm pm-e3 pi pi-e2   sq2 sq2-cw8 sq1 sq1-cw4')},
+        {...s({roll: -90, x: sl(3  ), z: sl(4 * 1.5)}, 'pc3 pc3-s1 pn pn-e3 po po-cw3  sq2 sq2-cw1 sq1 sq1-cw3')},
+
+        {...s({roll: -90, x: sl(0  ), z: sl(5 * 1.5)}, 'pc2 pc2-s3 ps ps-e2 po po-cw12 sq1 sq1-cw6')},
+        {...s({roll: -90, x: sl(1.5), z: sl(5 * 1.5)}, 'pc2 pc2-s2 pm pm-e2 pi pi-e1   sq1        ')},
+        {...s({roll: -90, x: sl(3  ), z: sl(5 * 1.5)}, 'pc2 pc2-s1 pn pn-e2 po po-cw4  sq1 sq1-cw2')},
+
+        {...s({roll: -90, x: sl(0  ), z: sl(6 * 1.5)}, 'pc1 pc1-s3 ps ps-e1 po po-cw15 sq1 sq1-cw7')},
+        {...s({roll: -90, x: sl(1.5), z: sl(6 * 1.5)}, 'pc1 pc1-s2 pm pm-e1 po po-cw16 sq1 sq1-cw8')},
+        {...s({roll: -90, x: sl(3  ), z: sl(6 * 1.5)}, 'pc1 pc1-s1 pn pn-e1 po po-cw1  sq1 sq1-cw1')},
+
     
-        // CEILING RAFTERS, #15 - #23:
+        // CEILING RAFTERS, #21 - #29:
         // First, 2 rows running along Z+ axis (progressing and facing east to west),
         // each 3 * 3 == 9 strips each, same X+ position
         {...s({yaw: 90, x: sl(0 * 1.5), z: sl(1)}, 'rn-e9 ro-cw9 sq3 sq3-cw3')},
@@ -55,7 +64,7 @@ function buildNagBugglerSaberOfLightFixtures() {
         {...s({yaw: 90, x: sl(0 * 1.5), z: sl(8)}, 'rn-e2 ro-cw2 sq1 sq1-cw2')},
         {...s({yaw: 90, x: sl(0 * 1.5), z: sl(9)}, 'rn-e1 ro-cw1 sq1 sq1-cw1')},
 
-        // #24 - #32
+        // #30 - #38
         {...s({yaw: 90, x: sl(2 * 1.5), z: sl(1)}, 'rs-e9 ro-cw13 sq3 sq3-cw7')},
         {...s({yaw: 90, x: sl(2 * 1.5), z: sl(2)}, 'rs-e8 ro-cw14 sq3 sq3-cw8')},
         {...s({yaw: 90, x: sl(2 * 1.5), z: sl(3)}, 'rs-e7 ro-cw15 sq3 sq3-cw9')},
@@ -67,19 +76,19 @@ function buildNagBugglerSaberOfLightFixtures() {
         {...s({yaw: 90, x: sl(2 * 1.5), z: sl(9)}, 'rs-e1 ro-cw21 sq1 sq1-cw9')},
 
         // Next, 4 columns running along X-axis, each 3 strips in length
-        // #33 - #35
+        // #39 - #41
         {...s({yaw: 180, x: sl(1), z: sl(0 * 3)}, 'rc4 rc4-s3 sq3 sq3-cw6')},
         {...s({yaw: 180, x: sl(2), z: sl(0 * 3)}, 'rc4 rc4-s2 sq3 sq3-cw5')},
         {...s({yaw: 180, x: sl(3), z: sl(0 * 3)}, 'rc4 rc4-s1 sq3 sq3-cw4')},
-        // #36 - #38
+        // #42 - #44
         {...s({yaw: 180, x: sl(1), z: sl(1 * 3)}, 'rc3 rc1-s3 sq2 sq2-cw6 sq3 sq3-cw10')},
         {...s({yaw: 180, x: sl(2), z: sl(1 * 3)}, 'rc3 rc1-s2 sq2 sq2-cw5 sq3 sq3-cw11')},
         {...s({yaw: 180, x: sl(3), z: sl(1 * 3)}, 'rc3 rc1-s1 sq2 sq2-cw4 sq3 sq3-cw12')},
-        // #39 - #41
+        // #45 - #47
         {...s({yaw: 180, x: sl(1), z: sl(2 * 3)}, 'rc2 rc1-s3 sq1 sq1-cw6 sq2 sq2-cw10')},
         {...s({yaw: 180, x: sl(2), z: sl(2 * 3)}, 'rc2 rc1-s2 sq1 sq1-cw5 sq2 sq2-cw11')},
         {...s({yaw: 180, x: sl(3), z: sl(2 * 3)}, 'rc2 rc1-s1 sq1 sq1-cw4 sq2 sq2-cw12')},
-        // #42 - #44
+        // #48 - #50
         {...s({yaw: 180, x: sl(1), z: sl(3 * 3)}, 'rc1 rc1-s3 sq1 sq1-cw10')},
         {...s({yaw: 180, x: sl(2), z: sl(3 * 3)}, 'rc1 rc1-s2 sq1 sq1-cw11')},
         {...s({yaw: 180, x: sl(3), z: sl(3 * 3)}, 'rc1 rc1-s1 sq1 sq1-cw12')},
@@ -92,7 +101,7 @@ function buildNagBugglerSaberOfLightFixtures() {
         //        \,    /,               =>    /\
         //     \,    /,                       /  \
         // 
-        // #45 - #48, #49 - #52
+        // #51 - #58
         {...s({yaw: -45,  x: 0,                 z: 0  },                        'x3-3 sq3 sq3-se4 x xo xo-cw3')},
         {...s({yaw: -45,  x: sl(.75),           z: sl(.75)  },                  'x3-3 sq3 sq3-se3 x xi xi-cw3')},
         {...s({yaw: +45,  x: 0,                 z: sl(3)  },                    'x4-3 sq3 sq3-nw1 x xo xo-cw4')},
@@ -102,7 +111,7 @@ function buildNagBugglerSaberOfLightFixtures() {
         {...s({yaw: +135, x: sl(3),             z: sl(3)  },                    'x1-3 sq3 sq3-se1 x xo xo-cw1')},
         {...s({yaw: +135, x: sl(3) - sl(.75),   z: sl(3) - sl(.75)  },          'x1-3 sq3 sq3-se2 x xi xi-cw1')},
 
-        // #53 - #56, #57 - #60
+        // #59 - #60
         {...s({yaw: -45,  x: 0,                 z: sl(3)  },                    'x3-2 sq2 sq2-se4 x xo xo-cw3')},
         {...s({yaw: -45,  x: sl(.75),           z: sl(3) + sl(.75)  },          'x3-2 sq2 sq2-se3 x xi xi-cw3')},
         {...s({yaw: +45,  x: 0,                 z: sl(3) + sl(3)  },            'x4-2 sq2 sq2-nw1 x xo xo-cw4')},
@@ -112,7 +121,7 @@ function buildNagBugglerSaberOfLightFixtures() {
         {...s({yaw: +135, x: sl(3),             z: sl(3) + sl(3)  },            'x1-2 sq2 sq2-se1 x xo xo-cw1')},
         {...s({yaw: +135, x: sl(3) - sl(.75),   z: sl(3) + sl(3) - sl(.75)  },  'x1-2 sq2 sq2-se2 x xi xi-cw1')},
 
-        // #61 - #64, #65 - #68
+        // #67 - #74
         {...s({yaw: -45,  x: 0,                 z: sl(6)  },                    'x3-1 sq1 sq1-se4 x xo xo-cw3')},
         {...s({yaw: -45,  x: sl(.75),           z: sl(6) + sl(.75)  },          'x3-1 sq1 sq1-se3 x xi xi-cw3')},
         {...s({yaw: +45,  x: 0,                 z: sl(6) + sl(3)  },            'x4-1 sq1 sq1-nw1 x xo xo-cw4')},
