dbbeced43dd529ae57e3dedfd5776db381642da4
Johnny Marnell <johnnymarnell@gmail.com>
2023-07-25 16:53:48 -0500

    copy pasta hack "running / enabled" bool param, for "FX" channel type overlay (?)

--- src/main/java/org/iqe/pattern/EqVisualizerPattern.java
+++ src/main/java/org/iqe/pattern/EqVisualizerPattern.java
@@ -4,16 +4,21 @@ import heronarts.lx.LX;
 import heronarts.lx.audio.GraphicMeter;
 import heronarts.lx.color.LXColor;
 import heronarts.lx.model.LXPoint;
+import heronarts.lx.parameter.BooleanParameter;
 import heronarts.lx.pattern.LXPattern;
 import org.iqe.Audio;
 
 public class EqVisualizerPattern extends LXPattern {
+    final BooleanParameter enabled = new BooleanParameter("running",true);
     public EqVisualizerPattern(LX lx) {
         super(lx);
+        addParameter(enabled);
     }
 
     @Override
     protected void run(double v) {
+        for (LXPoint p : model.points) colors[p.index] = LXColor.CLEAR;
+        if (!enabled.getValueb()) return;
         GraphicMeter graphicMeter = Audio.get().graphicMeter();
 
         for (LXPoint p : this.model.points) {
