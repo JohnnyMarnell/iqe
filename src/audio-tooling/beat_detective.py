c4afe1ee376d96bb9fe1a952c66e29d28152e035
Johnny Marnell <johnnymarnell@gmail.com>
2024-03-31 22:50:04 -0400

    Formatting, docs update, some clean up

--- src/audio-tooling/beat_detective.py
+++ src/audio-tooling/beat_detective.py
@@ -1,19 +1,31 @@
 import sys
 import time
 import threading
-import librosa
 import math
+import argparse
+from dataclasses import dataclass
+from pythonosc import udp_client
+import librosa
 import numpy as np
 from pyaudio_utils import Audio, Block
-from pythonosc import udp_client
-from dataclasses import dataclass
+
 
 @dataclass
-class Window: samples: np.ndarray; start_block: Block; end_block: Block;
+class Window:
+    """Sample window built from recently heard blocks. Could we cache these?"""
 
-class BeatDetective():
+    samples: np.ndarray
+    start_block: Block
+    end_block: Block
+
+
+class BeatDetective:
+    """Detect past beats using librosa, and fire events for upcoming ones"""
+
     def __init__(self, block_size=64, sample_rate=48000, seconds=5 * 60):
-        self.audio = Audio(block_size=block_size, sample_rate=sample_rate, seconds=seconds)
+        self.audio = Audio(
+            block_size=block_size, sample_rate=sample_rate, seconds=seconds
+        )
         self.osc = udp_client.SimpleUDPClient("127.0.0.1", 3232)
         self.predict_thread = None
         self.running = False
@@ -22,42 +34,51 @@ class BeatDetective():
         self.last_beat = 0
         self.last_update = None
         self.tempo_clear()
-        
+
     def fire_beat(self):
+        """ToDo:make these configurable event handlers"""
         self.osc.send_message("/lx/tempo/beat", 1.0)
-    
+
     def tempo_update(self, bpm, beat_time, now):
+        """ToDo:make these configurable event handlers"""
         # Set clock back to internal
         self.osc.send_message("/lx/tempo/bpm", self.bpm)
         self.osc.send_message("/lx/tempo/clockSource", 0)
-        
+
         self.bpm = bpm
-        self.spb = 60 / self.bpm
+        self.spb = 60 / self.bpm  # seconds per beat
         self.last_beat = beat_time + math.floor((now - beat_time) / self.spb) * self.spb
-        
+
         print(f"Tempo update sent for {bpm:2f} bpm")
 
     def tempo_clear(self):
+        """ToDo:make these configurable event handlers"""
         self.bpm = 0
         self.last_update = None
         # Temporarily set clock source to OSC (2), so it doesn't auto tick
         self.osc.send_message("/lx/tempo/clockSource", 2)
         print("Tempo cleared, clock stopped.")
-    
+
     def check_and_fire_beat(self):
+        """Called per tick, check if we need to fire a beat event"""
         if not self.bpm:
             return
         now = self.audio.time()
         if now - self.last_beat >= self.spb:
             self.fire_beat()
-            self.last_beat = self.last_beat + self.spb
-        
+            self.last_beat += self.spb
+
     def predict(self, secs_back=10, update=True):
+        """Make a prediction using librosa analysis of past, set next beat and update tempo if needed"""
+
         now = self.audio.time()
         start = now
         window = self.window(secs_back_start=secs_back)
-        bpm, beats = self.predict_beats(window.samples, sr=self.audio.sample_rate)
-        
+        bpm, beats = self.detect_recent_past_beats(
+            window.samples, sr=self.audio.sample_rate
+        )
+
+        # Clear the last tempo if we need to
         if not bpm or len(beats) < 1:
             if update:
                 print("Skipping this prediction update...")
@@ -69,39 +90,49 @@ class BeatDetective():
         if bpm >= 160:
             bpm = bpm / 2
         spb = 60 / bpm
-        
+
         # anchor_beat_index = 0
         anchor_beat_index = len(beats) - 1
-        beat_ests = [beat + (anchor_beat_index - k) * spb for k, beat in enumerate(beats)]
+        beat_ests = [
+            beat + (anchor_beat_index - k) * spb for k, beat in enumerate(beats)
+        ]
         anchor = np.mean(beat_ests)
-        beat_time = window.start_block.input_buffer_adc_time + anchor
-        
+        beat_time = window.start_block.time.input_buffer_adc_time + anchor
+
         if update or not self.last_update:
             self.tempo_update(bpm, beat_time, now)
         if update:
             self.last_update = now
-            
-        
+
         wsecs = len(window.samples) / self.audio.sample_rate
         avg_tempo = 60 / np.average(np.diff(beats))
         run_time = self.audio.time() - start
-        print(f"Window secs: {wsecs:.2f}, returned tempo {bpm:.2f}, avg {avg_tempo:.2f}, # beats {len(beats)}, in {run_time:.2f} secs")
-    
-    """ Rounded to blocks, build a mono, [-1, 1] normalized numpy array window of near desired length """
+        print(
+            f"Window secs: {wsecs:.2f}, returned tempo {bpm:.2f}, "
+            + f"avg {avg_tempo:.2f}, # beats {len(beats)}, in {run_time:.2f} secs"
+        )
+
+    def past_block(self, secs_back):
+        """Get a block from the past, some seconds back (shift by one for added insurance)"""
+        # ToDo: the frame buffer will wrap, problems?
+        spb = self.audio.sample_rate / self.audio.block_size
+        return self.audio.num_blocks_read - math.ceil(secs_back * spb) - 1
+
     def window(self, secs_back_start=10, secs_back_end=0):
-        end_block = self.audio.num_blocks_read - math.ceil(secs_back_end * self.audio.sample_rate / self.audio.block_size) - 1
-        start_block = self.audio.num_blocks_read - math.ceil(secs_back_start * self.audio.sample_rate / self.audio.block_size) - 1
-        start_block = max(start_block, 0)
-        
-        num_blocks = end_block - start_block + 1
-        # print(f"Num blocks read: {self.audio.num_blocks_read} about {self.audio.num_blocks_read * self.audio.block_size / self.audio.sample_rate} secs")
+        """Rounded to blocks, build a mono, [-1, 1] normalized numpy array window of near desired length"""
+
+        end_block = self.past_block(secs_back_end)
+        start_block = max(self.past_block(secs_back_start), 0)
+
+        # print(f"Num blocks read: {self.audio.num_blocks_read} about {self.audio.num_blocks_read \
+        #   * self.audio.block_size / self.audio.sample_rate} secs")
         # print(f"Start and end block {start_block} => {end_block}")
-        
+
         # First concat (hopefully fast) all the block bytes to one array (assuming 16 bit integers, 2 bytes)
         stream_bytes = bytearray()
         for i in range(start_block, end_block + 1):
             stream_bytes.extend(self.audio.blocks[i % self.audio.num_blocks].samples)
-        
+
         # Now convert this to [-1, 1] mono mixed numpy array for librosa
         audio_array = np.frombuffer(stream_bytes, dtype=np.int16)
         # Reshape the audio array into separate channels
@@ -112,20 +143,27 @@ class BeatDetective():
         max_sample = np.max(np.abs(mono_signal))
         if max_sample > 0:
             mono_signal = mono_signal / max_sample
-        return Window(samples=mono_signal,
-                      start_block=self.audio.blocks[start_block % self.audio.num_blocks],
-                      end_block=self.audio.blocks[end_block % self.audio.num_blocks])
 
-    def predict_beats(self, samples, sr, hop_length=256):
+        return Window(
+            samples=mono_signal,
+            start_block=self.audio.blocks[start_block % self.audio.num_blocks],
+            end_block=self.audio.blocks[end_block % self.audio.num_blocks],
+        )
+
+    def detect_recent_past_beats(self, samples, sr, hop_length=256):
+        """Use librosa to detect recent beats from the past"""
+
         predict_env = librosa.onset.onset_strength_multi
-        onset_env = predict_env(y=samples, sr=sr,
-                                hop_length=hop_length,
-                                aggregate=np.median, # default is mean
-                                lag=1, # default, unit? "time lag for computing differences"
-                                max_size=1, # default, do not filter freq bins
-                                detrend=False, # default, do not "filter onset strength to remove DC component"
-                                center=True, # Centered frame analysis in STFT, by hop length
-                            )
+        onset_env = predict_env(
+            y=samples,
+            sr=sr,
+            hop_length=hop_length,
+            aggregate=np.median,  # default is mean
+            lag=1,  # default, unit? "time lag for computing differences"
+            max_size=1,  # default, do not filter freq bins
+            detrend=False,  # default, do not "filter onset strength to remove DC component"
+            center=True,  # Centered frame analysis in STFT, by hop length
+        )
         onset_env = onset_env[..., 0, :]
         # HOP_LENGTH = 512
         # predict_env = librosa.onset.onset_strength
@@ -138,66 +176,127 @@ class BeatDetective():
         #                         center=True, # Centered frame analysis in STFT, by hop length
         #                         )
 
-        predict = librosa.beat.beat_track
-        return predict(onset_envelope=onset_env, sr=sr, units='time',
-                    hop_length=hop_length,
-                    tightness=1000, # yikers island, what does this do... good? 800 1000, bad 400 600 1600
-                    # start_bpm=126,
-                    #    trim=False,
-                    )
-    
+        return librosa.beat.beat_track(
+            onset_envelope=onset_env,
+            sr=sr,
+            units="time",
+            hop_length=hop_length,
+            tightness=1000,  # yikers island, what does this do... good? 800 1000, bad 400 600 1600
+            # start_bpm=126,
+            # trim=False,
+        )
+
     def predict_loop(self, predict_interval_secs=5, secs_back=None, update=True):
+        """Build a loop that runs predictions at an interval"""
+
         secs_back = predict_interval_secs if secs_back is None else secs_back
+
         def loop():
-            print(f"Starting prediction loop calculating every {predict_interval_secs} secs")
+            print(
+                f"Starting prediction loop calculating every {predict_interval_secs} secs"
+            )
             time.sleep(predict_interval_secs)
             while self.running:
                 start = time.time()
                 self.predict(secs_back=secs_back, update=update)
                 time.sleep(max(0, predict_interval_secs - (time.time() - start)))
+
         return loop
-    
-    def run(self, predict_interval_secs=5, secs_back=None, in_device='BlackHole 2ch', pipe=False, out_device='Speakers'):
-        self.predict_thread = threading.Thread(target=self.predict_loop(predict_interval_secs=predict_interval_secs, secs_back=secs_back))
-        self.predict_thread.start()
-        tempo_thread = threading.Thread(target=self.predict_loop(predict_interval_secs=1, secs_back=1.5, update=False))
-        tempo_thread.start()
-        
+
+    def run(
+        self,
+        predict_interval_secs=5,
+        secs_back=None,
+        in_device="BlackHole 2ch",
+        pipe=False,
+        out_device="Speakers",
+    ):
+        """Build a short and longer prediction loop and start them running in multiple threads"""
+
+        self.predict_thread = threading.Thread(
+            target=self.predict_loop(
+                predict_interval_secs=predict_interval_secs, secs_back=secs_back
+            )
+        )
+        tempo_thread = threading.Thread(
+            target=self.predict_loop(
+                predict_interval_secs=1, secs_back=1.5, update=False
+            )
+        )
+
         def in_callback(block):
             self.check_and_fire_beat()
+
         self.audio.listen(in_device, in_callback)
 
         if pipe:
+
             def out_callback(_, frame_count, block_time, status):
-                return self.audio.blocks[self.audio.num_blocks_written % self.audio.num_blocks].samples
-            out = self.audio.output(out_device, out_callback)
-        
+                return self.audio.latest_block_written().samples
+
+            self.audio.output(out_device, out_callback)
+
         self.running = True
+        self.predict_thread.start()
+        tempo_thread.start()
         while self.audio.input_stream.is_active():
             time.sleep(1)
-    
+
     def shutdown(self):
+        """Shutdown underlying audio"""
         self.running = False
         self.audio.shutdown()
-    
-    def dump_env_info(self):
-        self.log('Python version:' + sys.version)
-        self.log('****** Enumerating sys.modules:')
-        for module in sys.modules:
-            self.log(sys.modules[module])
-            self.log(module)
-        self.log('****** Package resources distros:')
-        for dist in __import__('pkg_resources').working_set:
-            self.log(dist.project_name.replace('Python', ''))
-    
-    def log(self, msg):
-        print(msg)
 
+    def dump_env_info(self):
+        """Debug info"""
+        print("Python version:" + sys.version)
+        print("****** Enumerating sys.modules:")
+        for module in sys.modules.items():
+            print(module)
+        print("****** Package resources distros:")
+        for dist in __import__("pkg_resources").working_set:
+            print(dist.project_name.replace("Python", ""))
+
+
+# Setup command arguments
+parser = argparse.ArgumentParser(
+    description="Beat detective: predict and fire beat events, e.g. with MIDI or OSC",
+    formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+)
+parser.add_argument(
+    "-i",
+    "--input",
+    type=str,
+    metavar="IN",
+    default="BlackHole 2ch",
+    help="Input to listen for beats",
+)
+parser.add_argument(
+    "-o",
+    "--output",
+    type=str,
+    metavar="OUT",
+    default="Speakers",
+    help="Output device name, if piping audio thru",
+)
+parser.add_argument(
+    "--pipe",
+    action=argparse.BooleanOptionalAction,
+    default=True,
+    help="Pipe audio through to output device",
+)
+
+args = parser.parse_args()
 
 beat_detective = BeatDetective(block_size=64, sample_rate=48000, seconds=30)
 # beat_detective.dump_env_info()
 try:
-    beat_detective.run(predict_interval_secs=10, secs_back=10,
-                        in_device='DUO')
+    beat_detective.run(
+        predict_interval_secs=10,
+        secs_back=10,
+        in_device=args.input,
+        pipe=args.pipe,
+        out_device=args.output,
+    )
 except KeyboardInterrupt as err:
     beat_detective.shutdown()
--- src/audio-tooling/pyaudio_utils.py
+++ src/audio-tooling/pyaudio_utils.py
@@ -1,15 +1,48 @@
-import pyaudio
+import logging
 import time
 import math
 from dataclasses import dataclass
+import pyaudio
+
+logger = logging.getLogger(__name__)
+
 
 @dataclass
-class Block: index: int; size: int; samples: bytearray; wall_time: float; stream_time: float; \
-                         input_buffer_adc_time: float; current_time: float; output_buffer_dac_time: float;
+class BlockTime:
+    """
+    Various timestamps that can occur during a block of audio processing
+    """
+
+    wall_time: float
+    stream_time: float
+    current_time: float
+    input_buffer_adc_time: float
+    output_buffer_dac_time: float
+
+
+@dataclass
+class Block:
+    """
+    A block, or buffer of audio data, collection of samples
+    """
+
+    index: int
+    size: int
+    samples: bytearray
+    time: BlockTime
+
 
 p = pyaudio.PyAudio()
 
+
 class Audio:
+    """
+    Main helper class to manage audio input and output
+    """
+
+    input_stream: pyaudio.PyAudio.Stream
+    output_stream: pyaudio.PyAudio.Stream
+
     def __init__(self, block_size=64, sample_rate=48000, seconds=5 * 60):
         self.pyaudio = p
         self.block_size = block_size
@@ -17,157 +50,202 @@ class Audio:
         self.block_time = self.block_size / self.sample_rate
         self.num_blocks = math.ceil(seconds * sample_rate / block_size)
         self.devices = self.query_devices()
-        self.blocks = list()
-        for i in range(self.num_blocks):
-            self.blocks.append(Block(index=0, size=0, samples=bytearray(2 * self.block_size), wall_time=0, stream_time=0,
-                              input_buffer_adc_time=0, current_time=0, output_buffer_dac_time=0))
+        self.blocks: list[Block] = []
+
         self.num_blocks_read = 0
         self.num_blocks_written = 0
-        self.output_stream = None
-        self.input_stream = None
-        self.input_channels = None
-        self.input_time = None
+        self.input_channels = 0
+        self.input_time = 0
         self.shutdown_called = False
-        
-    def query_devices(self):
-        devices = list()
-        for j in range(0, p.get_host_api_count()):    
-            api_info = p.get_host_api_info_by_index(j)
-            num_devices = api_info.get('deviceCount')
-            # print(f"Host API {j}: {api_info}")
 
+    def query_devices(self):
+        """Query all audio devices"""
+        devices = []
+        for j in range(0, p.get_host_api_count()):
+            api_info = p.get_host_api_info_by_index(j)
+            num_devices = int(api_info.get("deviceCount") or 0)
             for i in range(0, num_devices):
-                dev_info = p.get_device_info_by_host_api_device_index(j, i)
-                # print("Audio Device id ", i, " - ", dev_info)
-                devices.append({"api": api_info, "device": dev_info})        
+                dev_info = p.get_device_info_by_host_api_device_index(j, i).copy()
+                dev_info.update({"api": api_info})
+                devices.append(dev_info)
         return devices
 
-    def find_output(self, name, require=True):
-        devices = [d for d in self.devices if d["device"]["maxOutputChannels"] > 0
-                and name in d["device"]["name"]]
-        if require and not devices:
-            raise ValueError(f"Output device {name} not found")
-        return devices[0]
+    def find_output(self, name):
+        """Find output by name"""
+        for device in self.devices:
+            if device["maxOutputChannels"] > 0 and name in device["name"]:
+                return device
+        raise ValueError(f"Output device {name} not found")
 
-    def find_input(self, name, require=True):
-        devices = [d for d in self.devices if d["device"]["maxInputChannels"] > 0
-                and name in d["device"]["name"]]
-        if require and not devices:
-            raise ValueError(f"Input device {name} not found")
-        return devices[0]
-        
+    def find_input(self, name):
+        """Find input by name"""
+        for device in self.devices:
+            if device["maxInputChannels"] > 0 and name in device["name"]:
+                return device
+        raise ValueError(f"Input device {name} not found")
+
     def time(self):
+        """Get the current time, relating to the input stream"""
         return self.input_time
-    
+
     def listen(self, device_name, callback, channels=None):
-        device = self.find_input(device_name, require=True)
-        self.input_channels = channels or device["device"]["maxInputChannels"]
-        print(f"Listening to input {device_name}, {self.input_channels} channels")
-        
+        """Listen to input device and call callback with block of audio data"""
+
+        device = self.find_input(device_name)
+        self.input_channels = channels or device["maxInputChannels"]
+        print(
+            f"Listening to input {device_name}, {self.input_channels} channels, "
+            + f"requested sample rate {self.sample_rate} and block size {self.block_size}"
+        )
+
+        # Pre-allocate blocks
+        for _ in range(self.num_blocks):
+            self.blocks.append(
+                Block(
+                    index=0,
+                    size=0,
+                    samples=bytearray(self.input_channels * self.block_size),
+                    time=BlockTime(
+                        wall_time=0,
+                        stream_time=0,
+                        current_time=0,
+                        input_buffer_adc_time=0,
+                        output_buffer_dac_time=0,
+                    ),
+                )
+            )
+
         def wrapped_callback(data, frame_count, block_time, status):
             try:
                 if status:
-                    print("**** non zero status XRUN?", status)
+                    print("**** non zero status BUFFER OVER/UNDER RUN ?", status)
+
                 block = self.blocks[self.num_blocks_read % self.num_blocks]
+                block.index = self.num_blocks_read
+                block.size = frame_count
+
+                # Copy samples
                 if block.samples is None or len(block.samples) != len(data):
+                    logger.warning(
+                        "Varying block sizes: bs %s, d %s, fc %s, c %s",
+                        len(block.samples),
+                        len(data),
+                        frame_count,
+                        self.block_size,
+                    )
                     block.samples = bytearray(data)
                 else:
                     block.samples[:] = data
-                block.index = self.num_blocks_read
-                block.size = frame_count
-                block.wall_time = time.time()
-                block.input_buffer_adc_time = block_time["input_buffer_adc_time"]
-                block.output_buffer_dac_time = block_time["output_buffer_dac_time"]
-                block.current_time = block_time["current_time"]
-                block.stream_time = self.input_stream.get_time()
-                
-                self.input_time = block.input_buffer_adc_time + self.block_time
-                
+
+                # Update timestamps
+                bt = block.time
+                bt.wall_time = time.time()
+                bt.stream_time = self.input_stream.get_time()
+                bt.input_buffer_adc_time = block_time["input_buffer_adc_time"]
+                bt.output_buffer_dac_time = block_time["output_buffer_dac_time"]
+                bt.stream_time = block_time["current_time"]
+
+                self.input_time = bt.input_buffer_adc_time + self.block_time
+
                 callback(block)
-                
-                self.num_blocks_read = self.num_blocks_read + 1
+
+                self.num_blocks_read += 1
                 return (None, pyaudio.paContinue)
-            except Exception as err:
-                print("Error in input callback", err)
+
+            except Exception:
+                logger.exception("Error in input callback, aborting")
                 return (None, pyaudio.paAbort)
-        
+
         self.input_stream = p.open(
             rate=self.sample_rate,
             channels=self.input_channels,
             format=pyaudio.paInt16,
             input=True,
-            frames_per_buffer=self.block_size,
-            input_device_index=device["device"]["index"],
+            frames_per_buffer=self.block_size // 2,  # maybe 2 periods?
+            input_device_index=device["index"],
             stream_callback=wrapped_callback,
-            start=False
+            start=False,
         )
+
         self.input_stream.start_stream()
         return self.input_stream
-    
+
     def output(self, device_name, callback, channels=None):
+        """Register a callback to write to a output device stream"""
+
         device = self.find_output(device_name)
-        channels = channels or device["device"]["maxOutputChannels"]
-        print(f"Using output {device['device']['name']}, {channels} channels")
-        
+        channels = channels or device["maxOutputChannels"]
+        print(f"Using output {device['name']}, {channels} channels")
+
         def wrapped_callback(_, frame_count, block_time, status):
             try:
                 if status:
-                    print("**** non zero status XRUN?", status)
+                    print("**** non zero status BUFFER OVER/UNDER RUN ?", status)
+
                 samples = callback(_, frame_count, block_time, status)
-                self.num_blocks_written = self.num_blocks_written + 1
                 if isinstance(samples, bytearray):
                     samples = bytes(samples)
+
+                self.num_blocks_written = self.num_blocks_written + 1
                 return (samples, pyaudio.paContinue)
-            except Exception as err:
-                print("Error in output callback", err)
-                return (None, pyaudio.paAbort)        
-        
+
+            except Exception:
+                logger.exception("Error in output callback, aborting")
+                return (None, pyaudio.paAbort)
+
         self.output_stream = p.open(
             rate=self.sample_rate,
             channels=channels,
             format=pyaudio.paInt16,
             output=True,
-            frames_per_buffer=self.block_size,
-            output_device_index=device["device"]["index"],
+            frames_per_buffer=self.block_size // 2,  # maybe 2 periods?
+            output_device_index=device["index"],
             stream_callback=wrapped_callback,
-            start=False
+            start=False,
         )
         self.output_stream.start_stream()
         return self.output_stream
-    
+
     def latest_block_written(self):
+        """Return the last block written of the ring buffer"""
         return self.blocks[self.num_blocks_written % self.num_blocks]
-        
+
     def shutdown(self):
+        """Shutdown, halt PyAudio processes"""
         if self.shutdown_called:
             return
         self.shutdown_called = True
         print("Shutting down audio...")
-        if self.output_stream:
+        if self.output_stream is not None:
             self.output_stream.close()
-        if self.input_stream:
+        if self.input_stream is not None:
             self.input_stream.close()
         p.terminate()
 
 
 # A Simple test
-if __name__ == '__main__':
+if __name__ == "__main__":
+    print("Look for sys audio BlackHole and pipe to Speakers output")
+    audio = None
     try:
-        print("Will look for and listen to BlackHole loopback'd audio and pipe to Speakers output")
         audio = Audio()
-        
+
         def in_callback(block):
-            pass
-        audio.listen('BlackHole 2ch', in_callback)
+            """We don't need to do anything else, input is being stored"""
 
         def out_callback(_, frame_count, block_time, status):
+            """Return the next block, thus piped / echoed to"""
             return audio.latest_block_written().samples
-        out = audio.output('Speakers', out_callback)
-        
+
+        audio.listen("BlackHole 2ch", in_callback)
+        audio.output("Speakers", out_callback)
+
         while audio.input_stream.is_active():
             time.sleep(1)
 
-    except KeyboardInterrupt as err:
-        audio.shutdown()
+    except KeyboardInterrupt:
+        if audio is not None:
+            audio.shutdown()
     finally:
-        audio.shutdown()
+        if audio is not None:
+            audio.shutdown()
